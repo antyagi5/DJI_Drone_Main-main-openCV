512c9627f6c2a08d8435057d66ca5a0097d9880a
antyagi5 <>
2021-08-09 18:23:09 -0400

    Added OpenCV intializations, processing frames, disabled texture view and added image view

---
+++ app/src/main/java/com/dji/GSDemo/GoogleMap/CameraActivity.java
@@ -0,0 +1,481 @@
+package com.dji.GSDemo.GoogleMap;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.SurfaceTexture;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.TextureView.SurfaceTextureListener;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.ImageView;
+
+import com.dji.GSDemo.GoogleMap.cnn.CNNExtractorService;
+import com.dji.GSDemo.GoogleMap.cnn.impl.CNNExtractorServiceImpl;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.dnn.Net;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.core.CvType;
+import org.opencv.android.Utils;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import dji.common.camera.SettingsDefinitions;
+import dji.common.camera.SystemState;
+import dji.common.error.DJIError;
+import dji.common.product.Model;
+import dji.common.useraccount.UserAccountState;
+import dji.common.util.CommonCallbacks;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.camera.Camera;
+import dji.sdk.camera.VideoFeeder;
+import dji.sdk.codec.DJICodecManager;
+import dji.sdk.useraccount.UserAccountManager;
+
+public class CameraActivity extends org.opencv.android.CameraActivity implements SurfaceTextureListener,OnClickListener {
+
+    private static final String TAG = MainActivity.class.getName();
+    protected VideoFeeder.VideoDataListener mReceivedVideoDataListener = null;
+    // Codec for video live view
+    protected DJICodecManager mCodecManager = null;
+
+    protected TextureView mVideoSurface = null;
+    private Button mCaptureBtn, mShootPhotoModeBtn, mRecordVideoModeBtn, mMapBtn;
+    private ToggleButton mRecordBtn;
+    private TextView recordingTime;
+    private ImageView mImag;
+
+    private Handler handler;
+
+    // opencv
+    private static final String IMAGENET_CLASSES = "imagenet_classes.txt";
+    private static final String MODEL_FILE = "pytorch_mobilenet.onnx";
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private Net opencvNet;
+
+    private CNNExtractorService cnnService;
+
+    // opencv
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG,"OpenCV loaded successfully!");
+                    //mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        handler = new Handler();
+
+        initUI();
+
+        // The callback for receiving the raw H264 video data for camera live view
+        mReceivedVideoDataListener = new VideoFeeder.VideoDataListener() {
+
+            @Override
+            public void onReceive(byte[] videoBuffer, int size) {
+                if (mCodecManager != null) {
+                    mCodecManager.sendDataToDecoder(videoBuffer, size);
+                }
+            }
+        };
+
+        // OpenCV manager initialization
+        OpenCVLoader.initDebug();
+        //mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+
+        // initialize implementation of CNNExtractorService
+        this.cnnService = new CNNExtractorServiceImpl();
+
+        // Neural Network model file reading
+        // obtaining converted network
+        String onnxModelPath = getPath(MODEL_FILE, this);
+        if (onnxModelPath.trim().isEmpty()) {
+            Log.i(TAG, "Failed to get model file");
+            return;
+        }
+        opencvNet = cnnService.getConvertedNet(onnxModelPath, TAG);
+
+        Camera camera = FPVApplication.getCameraInstance();
+
+        if (camera != null) {
+
+            camera.setSystemStateCallback(new SystemState.Callback() {
+                @Override
+                public void onUpdate(SystemState cameraSystemState) {
+                    if (null != cameraSystemState) {
+
+                        int recordTime = cameraSystemState.getCurrentVideoRecordingTimeInSeconds();
+                        int minutes = (recordTime % 3600) / 60;
+                        int seconds = recordTime % 60;
+
+                        final String timeString = String.format("%02d:%02d", minutes, seconds);
+                        final boolean isVideoRecording = cameraSystemState.isRecording();
+
+                        CameraActivity.this.runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+
+                                recordingTime.setText(timeString);
+
+                                /*
+                                 * Update recordingTime TextView visibility and mRecordBtn's check state
+                                 */
+                                if (isVideoRecording){
+                                    recordingTime.setVisibility(View.VISIBLE);
+                                }else
+                                {
+                                    recordingTime.setVisibility(View.INVISIBLE);
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+
+        }
+    }
+
+    protected void onProductChange() {
+        initPreviewer();
+        loginAccount();
+    }
+
+    private void loginAccount(){
+
+        UserAccountManager.getInstance().logIntoDJIUserAccount(this,
+                new CommonCallbacks.CompletionCallbackWith<UserAccountState>() {
+                    @Override
+                    public void onSuccess(final UserAccountState userAccountState) {
+                        Log.e(TAG, "Login Success");
+                    }
+                    @Override
+                    public void onFailure(DJIError error) {
+                        showToast("Login Error:"
+                                + error.getDescription());
+                    }
+                });
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    private static String getPath(String file, Context context) {
+        AssetManager assetManager = context.getAssets();
+        BufferedInputStream inputStream;
+        try {
+            // read the defined data from assets
+            inputStream = new BufferedInputStream(assetManager.open(file));
+            byte[] data = new byte[inputStream.available()];
+            inputStream.read(data);
+            inputStream.close();
+            // Create copy file in storage.
+            File outFile = new File(context.getFilesDir(), file);
+            FileOutputStream os = new FileOutputStream(outFile);
+            os.write(data);
+            os.close();
+            // Return a path to file which may be read in common way.
+            return outFile.getAbsolutePath();
+        } catch (IOException ex) {
+            Log.i(TAG, "Failed to upload a file");
+        }
+        return "";
+    }
+
+    @Override
+    public void onResume() {
+        Log.e(TAG, "onResume");
+        super.onResume();
+        //initPreviewer();
+        //onProductChange();
+
+        if(mVideoSurface == null) {
+            Log.e(TAG, "mVideoSurface is null");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        Log.e(TAG, "onPause");
+        //uninitPreviewer();
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.e(TAG, "onStop");
+        super.onStop();
+    }
+
+    public void onReturn(View view){
+        Log.e(TAG, "onReturn");
+        this.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.e(TAG, "onDestroy");
+        uninitPreviewer();
+        super.onDestroy();
+    }
+
+    private void initUI() {
+        // init mVideoSurface
+        mVideoSurface = (TextureView)findViewById(R.id.video_previewer_surface);
+        mImag = (ImageView) findViewById(R.id.processedframes);
+
+        recordingTime = (TextView) findViewById(R.id.timer);
+        mCaptureBtn = (Button) findViewById(R.id.btn_capture);
+        mRecordBtn = (ToggleButton) findViewById(R.id.btn_record);
+        mShootPhotoModeBtn = (Button) findViewById(R.id.btn_shoot_photo_mode);
+        mRecordVideoModeBtn = (Button) findViewById(R.id.btn_record_video_mode);
+        mMapBtn = (Button) findViewById(R.id.btn_map);
+
+        if (null != mVideoSurface) {
+            mVideoSurface.setSurfaceTextureListener(this);
+        }
+
+        mCaptureBtn.setOnClickListener(this);
+        mRecordBtn.setOnClickListener(this);
+        mShootPhotoModeBtn.setOnClickListener(this);
+        mRecordVideoModeBtn.setOnClickListener(this);
+        mMapBtn.setOnClickListener(this);
+
+        recordingTime.setVisibility(View.INVISIBLE);
+
+        mRecordBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    startRecord();
+                } else {
+                    stopRecord();
+                }
+            }
+        });
+    }
+
+    private void initPreviewer() {
+
+        BaseProduct product = FPVApplication.getProductInstance();
+
+        if (product == null || !product.isConnected()) {
+            showToast(getString(R.string.disconnected));
+        } else {
+            if (null != mVideoSurface) {
+                mVideoSurface.setSurfaceTextureListener(this);
+            }
+            if (!product.getModel().equals(Model.UNKNOWN_AIRCRAFT)) {
+                VideoFeeder.getInstance().getPrimaryVideoFeed().addVideoDataListener(mReceivedVideoDataListener);
+            }
+        }
+    }
+
+    private void uninitPreviewer() {
+        Camera camera = FPVApplication.getCameraInstance();
+        if (camera != null){
+            // Reset the callback
+            VideoFeeder.getInstance().getPrimaryVideoFeed().addVideoDataListener(null);
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Log.e(TAG, "onSurfaceTextureAvailable");
+        if (mCodecManager == null) {
+            mCodecManager = new DJICodecManager(this, surface, width, height);
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        Log.e(TAG, "onSurfaceTextureSizeChanged");
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        Log.e(TAG,"onSurfaceTextureDestroyed");
+        if (mCodecManager != null) {
+            mCodecManager.cleanSurface();
+            mCodecManager = null;
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        final Bitmap frame = mVideoSurface.getBitmap();
+        //Do whatever you want with the bitmap image here on every frame
+        Mat tmp = new Mat (frame.getHeight(), frame.getWidth(), CvType.CV_8UC1);
+        String classesPath = getPath(IMAGENET_CLASSES, this);
+        String predictedClass = cnnService.getPredictedLabel(tmp, opencvNet, classesPath);
+        // place the predicted label on the image
+        Imgproc.putText(tmp, predictedClass, new Point(200, 100), Imgproc.FONT_HERSHEY_SIMPLEX, 2, new Scalar(255, 121, 0), 3);
+
+        Bitmap img_bitmap = Bitmap.createBitmap(tmp.cols(), tmp.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(tmp, img_bitmap);
+        mImag.setImageBitmap(img_bitmap);
+    }
+
+    public void showToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(CameraActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btn_capture:{
+                captureAction();
+                break;
+            }
+            case R.id.btn_shoot_photo_mode:{
+                switchCameraMode(SettingsDefinitions.CameraMode.SHOOT_PHOTO);
+                break;
+            }
+            case R.id.btn_record_video_mode:{
+                switchCameraMode(SettingsDefinitions.CameraMode.RECORD_VIDEO);
+                break;
+            }
+            case R.id.btn_map:
+                startActivity(new Intent(this, Waypoint1Activity.class));
+            default:
+                break;
+        }
+    }
+
+    private void switchCameraMode(SettingsDefinitions.CameraMode cameraMode){
+
+        Camera camera = FPVApplication.getCameraInstance();
+        if (camera != null) {
+            camera.setMode(cameraMode, new CommonCallbacks.CompletionCallback() {
+                @Override
+                public void onResult(DJIError error) {
+
+                    if (error == null) {
+                        showToast("Switch Camera Mode Succeeded");
+                    } else {
+                        showToast(error.getDescription());
+                    }
+                }
+            });
+        }
+    }
+
+    // Method for taking photo
+    private void captureAction(){
+        final Camera camera = FPVApplication.getCameraInstance();
+        if (camera != null) {
+
+            SettingsDefinitions.ShootPhotoMode photoMode = SettingsDefinitions.ShootPhotoMode.SINGLE; // Set the camera capture mode as Single mode
+            camera.setShootPhotoMode(photoMode, new CommonCallbacks.CompletionCallback(){
+                @Override
+                public void onResult(DJIError djiError) {
+                    if (null == djiError) {
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                camera.startShootPhoto(new CommonCallbacks.CompletionCallback() {
+                                    @Override
+                                    public void onResult(DJIError djiError) {
+                                        if (djiError == null) {
+                                            showToast("take photo: success");
+                                        } else {
+                                            showToast(djiError.getDescription());
+                                        }
+                                    }
+                                });
+                            }
+                        }, 2000);
+                    }
+                }
+            });
+        }
+    }
+
+    // Method for starting recording
+    private void startRecord(){
+
+        final Camera camera = FPVApplication.getCameraInstance();
+        if (camera != null) {
+            camera.startRecordVideo(new CommonCallbacks.CompletionCallback(){
+                @Override
+                public void onResult(DJIError djiError)
+                {
+                    if (djiError == null) {
+                        showToast("Record video: success");
+                    }else {
+                        showToast(djiError.getDescription());
+                    }
+                }
+            }); // Execute the startRecordVideo API
+        }
+    }
+
+    // Method for stopping recording
+    private void stopRecord(){
+
+        Camera camera = FPVApplication.getCameraInstance();
+        if (camera != null) {
+            camera.stopRecordVideo(new CommonCallbacks.CompletionCallback(){
+
+                @Override
+                public void onResult(DJIError djiError)
+                {
+                    if(djiError == null) {
+                        showToast("Stop recording: success");
+                    }else {
+                        showToast(djiError.getDescription());
+                    }
+                }
+            }); // Execute the stopRecordVideo API
+        }
+
+    }
+}
---
+++ app/src/main/java/com/dji/GSDemo/GoogleMap/MainActivity.java
@@ -0,0 +1,31 @@
+package com.dji.GSDemo.GoogleMap;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View.OnClickListener clickListener = v -> {
+        switch (v.getId()) {
+            case R.id.btn_waypoint1:
+                startActivity(MainActivity.this, Waypoint1Activity.class);
+                break;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_waypoint1).setOnClickListener(clickListener);
+    }
+
+    public static void startActivity(Context context, Class activity) {
+        Intent intent = new Intent(context, activity);
+        context.startActivity(intent);
+    }
+}
